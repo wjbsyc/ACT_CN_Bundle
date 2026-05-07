327e611c78f8cc3173aab6f5dc13794cf73b2e66
Homura <wjbsyc@hotmail.com>
2026-05-08 00:02:21 +0800

    7.5 hot patch

--- cactbot/dist/util/coverage/coverage.bundle.js
+++ cactbot/dist/util/coverage/coverage.bundle.js
@@ -25255,6 +25255,7 @@ const kPrefixToCategory = {
   },
   '99-custom': {
     en: 'Custom Developer Triggers',
+    de: 'Benutzerdefinierte Entwickler Trigger',
     cn: '自定义开发者触发器',
     ko: '커스텀 개발자 트리거'
   },
